411291a4882639aef8e49b55e7d2c6401fdb8427
vineethadevika <84488013+vineethadevika@users.noreply.github.com>
2024-04-28 21:50:13 +0530

    Update settings.py

--- EXAMSEATINGAUTOGENERATEDSYSTEM/settings.py
+++ EXAMSEATINGAUTOGENERATEDSYSTEM/settings.py
@@ -116,13 +116,7 @@ USE_I18N = True
 USE_TZ = True
 
 # mail Code
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = "devikan4353@gmail.com"
-EMAIL_HOST_PASSWORD = "whjcctyndfcrvzyw"
-EMAIL_PORT = '587'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
